5a93d35acd799ecf9627fe885b82cfcc4ba64242
Alex Lovegrove <alex_lovegrove@live.co.uk>
2022-02-10 14:10:01 +0000

    general

--- best/config/environment.js
+++ best/config/environment.js
@@ -1,7 +1,3 @@
 export const port = 4000
 export const dbURI = 'mongodb://127.0.0.1:27017/sei61'
-<<<<<<< HEAD
 export const secret = '0Vyzy6fv1G^LZB%O#pYW' // random string
-=======
-export const secret = 'test'
->>>>>>> development
