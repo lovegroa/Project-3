e768e94abfe5c383c24373bb9cf78fac407efee2
Alex Lovegrove <alex_lovegrove@live.co.uk>
2022-02-17 10:55:43 +0000

    updated env

--- best/client/src/components/AddQuestion.js
+++ best/client/src/components/AddQuestion.js
@@ -6,19 +6,18 @@ import { useNavigate } from 'react-router-dom'
 import { getTokenFromLocalStorage } from './utils/userAuthenticated'
 
 const apiKey = process.env.REACT_APP_GOOGLE_SEARCH_API_KEY
-
+console.log(apiKey)
 const AddQuestion = () => {
-  
   const navigate = useNavigate()
 
-  const [ formData, setFormData ] = useState({
-    questionText: '', 
+  const [formData, setFormData] = useState({
+    questionText: '',
     imageUrl: '',
-    categories: [],
+    categories: []
   })
 
-  const [ error, setError ] = useState({
-    questionText: '', 
+  const [error, setError] = useState({
+    questionText: '',
     imageUrl: '',
     categories: ''
   })
@@ -26,19 +25,23 @@ const AddQuestion = () => {
   // call image API
   const getImage = async () => {
     const imgUrls = []
-    
+
     if (!formData.questionText) {
-      setError({ imageUrl: 'A question is required before you can add an image' })
+      setError({
+        imageUrl: 'A question is required before you can add an image'
+      })
     }
     try {
       if (formData.questionText) {
-        const { data } = await axios.get(`https://www.googleapis.com/customsearch/v1?q=${formData.questionText}&key=${apiKey}&cx=5f017c0fcf7051673&searchType=image`)
+        const { data } = await axios.get(
+          `https://www.googleapis.com/customsearch/v1?q=${formData.questionText}&key=${apiKey}&cx=5f017c0fcf7051673&searchType=image`
+        )
         console.log(data.items)
-        data.items.forEach(item => {
+        data.items.forEach((item) => {
           imgUrls.push(item.link)
         })
         const randomIndex = Math.floor(Math.random() * imgUrls.length)
-        setFormData({ ...formData, imageUrl: imgUrls[randomIndex]})
+        setFormData({ ...formData, imageUrl: imgUrls[randomIndex] })
       }
     } catch (error) {
       console.log(error.response.data.errors)
@@ -48,14 +51,14 @@ const AddQuestion = () => {
 
   // placeholder for extracting chosen categories using handleChange on React-Select
 
-  
-
   // handle search query
   const searchQuery = (e) => {
     setFormData({ ...formData, questionText: e.target.value.toLowerCase() })
     setError({ imageUrl: '' })
     if (e.target.value.toLowerCase().length > 30) {
-      setError({ questionText: 'The maximum length of a question is 30 characters' })
+      setError({
+        questionText: 'The maximum length of a question is 30 characters'
+      })
     }
   }
 
@@ -76,72 +79,76 @@ const AddQuestion = () => {
     }
   }
 
-
-
-  
   return (
     <>
-      <Container className='mt-5 input-form'> 
+      <Container className='mt-5 input-form'>
         <h4>Add a question...</h4>
-        
+
         <Form onSubmit={handleSubmit}>
           <Form.Group as={Row} className='mt-5 question'>
-            <Form.Label column sm={2} className='mb3'><span id='question'>What&#39;s the Best:</span></Form.Label>
+            <Form.Label column sm={2} className='mb3'>
+              <span id='question'>What&#39;s the Best:</span>
+            </Form.Label>
             <Col sm={10}>
-              <Form.Control  
-                as='textarea' rows={2}
+              <Form.Control
+                as='textarea'
+                rows={2}
                 name='questionText'
                 placeholder='Write your question here'
-                onChange={searchQuery} 
-              /> 
+                onChange={searchQuery}
+              />
               <Form.Text className='error'>
-              {error && 
-                <Form.Text>
-                  {error.imageUrl && error.imageUrl}
-                  {error.questionText && error.questionText}
-                </Form.Text>}         
+                {error && (
+                  <Form.Text>
+                    {error.imageUrl && error.imageUrl}
+                    {error.questionText && error.questionText}
+                  </Form.Text>
+                )}
               </Form.Text>
-              <hr className='mt-5'/>
+              <hr className='mt-5' />
             </Col>
           </Form.Group>
           <Form.Group as={Row} className='categories'>
-            <Form.Label column sm={2}><span id='image'>Add categories:</span></Form.Label>
-              <Col sm={10}>
-                {formData.questionText ?
-                <>
-                  {AnimatedMulti()}
-                </>
-                :
-                <div id='empty-div'>
-                </div>
-                }
-                <hr className='mt-5 mb-5'/>
-              </Col>
-              
+            <Form.Label column sm={2}>
+              <span id='image'>Add categories:</span>
+            </Form.Label>
+            <Col sm={10}>
+              {formData.questionText ? (
+                <>{AnimatedMulti()}</>
+              ) : (
+                <div id='empty-div'></div>
+              )}
+              <hr className='mt-5 mb-5' />
+            </Col>
           </Form.Group>
           <Form.Group as={Row} className='image'>
-            <Form.Label column sm={2} className='mb3'><span id='image'>Add an image:</span></Form.Label>
-              <Col sm={3}>
-                <Button variant='secondary' onClick={getImage}>Get random image</Button>
-              </Col>
-              <Col sm={7} className='d-flex flex-row-reverse'>
-                {formData.imageUrl &&
-                <Image className='img-fluid img-thumbnail q-image' src={formData.imageUrl}></Image> 
-                }
-                
-              </Col>
-              <hr className='mt-5 mb-5'/>
-          
+            <Form.Label column sm={2} className='mb3'>
+              <span id='image'>Add an image:</span>
+            </Form.Label>
+            <Col sm={3}>
+              <Button variant='secondary' onClick={getImage}>
+                Get random image
+              </Button>
+            </Col>
+            <Col sm={7} className='d-flex flex-row-reverse'>
+              {formData.imageUrl && (
+                <Image
+                  className='img-fluid img-thumbnail q-image'
+                  src={formData.imageUrl}
+                ></Image>
+              )}
+            </Col>
+            <hr className='mt-5 mb-5' />
           </Form.Group>
 
           <Form.Group className='text-center submit-btn'>
-            <Button variant='primary' type='submit'>Submit</Button>
+            <Button variant='primary' type='submit'>
+              Submit
+            </Button>
           </Form.Group>
-
         </Form>
       </Container>
     </>
-
   )
 }
 
