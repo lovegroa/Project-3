3e488d32d80076cccbbedcd8ebb6d27821576647
Steve Dobbie <steven.dobbie.scotland@gmail.com>
2022-02-18 11:43:32 +0000

    category search implemented

--- best/client/src/components/Questions.js
+++ best/client/src/components/Questions.js
@@ -7,13 +7,15 @@ import { MultiSelect } from './utils/reactSelect'
 
 const Questions = () => {
   
+  const [ questions, setQuestions ] = useState([])
+  const [ filterQuestions, setFilterQuestions ] = useState([])
   const [ searchResults, setSearchResults ] = useState([])
   const { searchTerm } = useParams()
   const [ selected, setSelected ] = useState({
     value: '', 
     label: ''
   })
-  const [ appliedFilter, setAppliedFilter ] = useState([])
+  //const [ appliedFilter, setAppliedFilter ] = useState([])
 
   useEffect(() => {
     const getQuestions = async () => {
@@ -27,6 +29,32 @@ const Questions = () => {
     getQuestions()
   }, [searchTerm])
 
+  useEffect(() => {
+    const getQuestions = async () => {
+      try {
+        const { data } = await axios.get('/api/questions/')
+        setQuestions(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getQuestions()
+  }, [])
+
+  useEffect(() => {
+    // apply category filters
+    if (questions.length && selected.length) {
+      const filterList = []
+      for (let i = 0; i < selected.length; i++) {
+        const filtersToApply = questions.filter((q) => {
+          return q.category === selected[i].label
+        })
+        filterList.push(...filtersToApply)
+        console.log(filterList)
+      }
+      setFilterQuestions(filterList)
+    }
+  }, [questions, selected])
 
   return (
     
@@ -59,38 +87,77 @@ const Questions = () => {
             <MultiSelect {...{ setSelected }} />
           </Col>
         </Row>
-        <Row className='justify-content-md-center m-4'>
+        <div id='q-empty-div'>
+        {selected.length &&
+        <>
+          <Row className='justify-content-md-center m-4'>
             <Col lg='12'>
-              <h4 className='text-left'>Search result for <span id='search-term'>{searchTerm}</span></h4>
+              <h4 className='text-left'>Search results</h4>
             </Col>
-        </Row>
-        
-        <Row >
-        {searchResults && searchResults.map(question => {
-          const { _id, questionText, imageUrl, voteCount } = question
-          const capitalQ = questionText
-              .split(' ')[0]
-              .split('')[0]
-              .toLocaleUpperCase()
-            const joinedQ = questionText.slice(1)
-            const parsedQ = capitalQ.concat(joinedQ)
-          return (
-            
-              <Col key={_id} md={6} lg={4}>
-                <Link className='links' to={`/question/${_id}`}>
-                  <div className='q-container'>
-                    <div className='q-image-container'>
-                      <img className='q-image' src={imageUrl} alt={parsedQ} />
-                      <div className='q-text'>{parsedQ}</div>
-                    </div>
-                  </div>
-                </Link>
-              </Col>
-              ) 
+          </Row>
+          <Row>
+            {(filterQuestions.length && selected.length) && filterQuestions.map((question) => {
+              const { _id, questionText, imageUrl } = question
+              const capitalQ = questionText
+                    .split(' ')[0]
+                    .split('')[0]
+                    .toLocaleUpperCase()
+              const joinedQ = questionText.slice(1)
+              const parsedQ = capitalQ.concat(joinedQ)
+                return (
+                  <Col key={_id} md={6} lg={4}>
+                    <Link className='links' to={`/question/${_id}`}>
+                      <div className='q-container'>
+                        <div className='q-image-container'>
+                          <img className='q-image' src={imageUrl} alt={parsedQ} />
+                          <div className='q-text'>{parsedQ}</div>
+                        </div>
+                      </div>
+                    </Link>
+                  </Col>
+                )
+              })
             }
-          )
-        }
-        </Row>
+          </Row>
+        </>
+        } 
+        </div>
+        {!selected.length &&
+        <>
+          <Row className='justify-content-md-center m-4'>
+              <Col lg='12'>
+                <h4 className='text-left'>Search result for <span id='search-term'>{searchTerm}</span></h4>
+              </Col>
+          </Row>
+          
+          <Row >
+          {searchResults && searchResults.map(question => {
+            const { _id, questionText, imageUrl } = question
+            const capitalQ = questionText
+                .split(' ')[0]
+                .split('')[0]
+                .toLocaleUpperCase()
+              const joinedQ = questionText.slice(1)
+              const parsedQ = capitalQ.concat(joinedQ)
+            return (
+              
+                <Col key={_id} md={6} lg={4}>
+                  <Link className='links' to={`/question/${_id}`}>
+                    <div className='q-container'>
+                      <div className='q-image-container'>
+                        <img className='q-image' src={imageUrl} alt={parsedQ} />
+                        <div className='q-text'>{parsedQ}</div>
+                      </div>
+                    </div>
+                  </Link>
+                </Col>
+                ) 
+              }
+            )
+          }
+          </Row>
+        </>
+      }
       </Container>
     
     
